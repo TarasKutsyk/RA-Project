cb518cdfbe0c3977acbdf1cc13578223f8131603
xsv7y <taras.kutsyk.pm.2019@lpnu.ua>
2022-12-01 21:59:19 +0200

    Dataset visualization added

--- data/dataset.py
+++ data/dataset.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import os
 
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 abs_path = os.path.abspath(os.path.join(
                   os.path.dirname(__file__), 
                   os.pardir))
@@ -44,3 +47,25 @@ if __name__ == '__main__':
 
     print('Dataset: ', data, sep='\n')
     print('Y (observations) values: ', y, sep='\n')
+
+    # visualize dataset:
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # x1 - Year
+    # x2 - Deaths count
+    # x3 - Duration
+    x1, x2, x3 = numerical_data.T
+
+    ax.scatter(x1, x2, x3,
+           linewidths=1, alpha=.7,
+           edgecolor='k',
+           s = 200,
+           c=y)
+
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Deaths Count')
+    ax.set_zlabel('Duration')
+
+    plt.show()
+
